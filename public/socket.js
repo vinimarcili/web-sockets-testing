10abc22871e7e49d9e8525b489cc6f767b4ddd87
Vinícius Marcili <vinipetter@gmail.com>
2024-06-05 16:21:30 -0300

    refactor: Update socket event names for consistency

--- public/socket.js
+++ public/socket.js
@@ -3,10 +3,10 @@ import { updateEditor } from "./document.js"
 const socket = io("http://localhost:4000")
 
 function emitEditorWrite(text) {
-  socket.emit("editor_write", text)
+  socket.emit("editor", text)
 }
 
-socket.on("editor_read", (text) => {
+socket.on("editor", (text) => {
   updateEditor(text)
 })
 
--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import { io } from "./server.js"
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id)
 
-  socket.on('editor_write', (text) => {
-    socket.broadcast.emit('editor_read', text)
+  socket.on('editor', (text) => {
+    socket.broadcast.emit('editor', text)
   })
 })
