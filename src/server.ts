36624367f863b7a395137a4206714fafdac36dcd
Vinícius Marcili <vinipetter@gmail.com>
2024-06-05 16:03:21 -0300

    chore: Add socket.io dependency and implement socket connection in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app: Express = express()
 const PORT: number = Number(process.env.PORT) || 4000
@@ -10,6 +12,14 @@ const publicFolder: string = path.join(__dirname, '../public')
 
 app.use(express.static(publicFolder))
 
-app.listen(PORT, () => {
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
+
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
