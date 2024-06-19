c95c9eec452fea67d9042d73a71328c786793342
Vinícius Marcili <vinipetter@gmail.com>
2024-06-19 14:55:48 -0300

    refactor: Update password validation and hashing in password-security.ts
    The code changes in this commit refactor the `validatePassword` and `hashPassword` functions in the `password-security.ts` file. The `validatePassword` function now uses the `timingSafeEqual` function from the `crypto` module to compare the hashed password with the provided password in a timing-safe manner. This improves the security of password validation. Additionally, the `hashPassword` function now includes the `timingSafeEqual` function in the import statement. This ensures that the `timingSafeEqual` function is available for use in the `validatePassword` function.

--- src/utils/password-security.ts
+++ src/utils/password-security.ts
@@ -1,17 +1,14 @@
-import { randomBytes, scryptSync } from "crypto"
+import { randomBytes, scryptSync, timingSafeEqual } from "crypto"
 
 export function hashPassword(password: string) {
   const sal = randomBytes(16).toString('hex')
-
   const hash = scryptSync(password, sal, 64).toString('hex')
-
   return `${sal}:${hash}`
 }
 
 export function validatePassword(password: string, salHash: string) {
   const [sal, hash] = salHash.split(':')
-
-  const newHash = scryptSync(password, sal, 64).toString('hex')
-
-  return newHash === hash
+  const testingHash = scryptSync(password, sal, 64)
+  const realHash = Buffer.from(hash, 'hex')
+  return timingSafeEqual(testingHash, realHash)
 }
